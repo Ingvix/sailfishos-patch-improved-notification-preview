cf9d3eb8731f08278041c1b396f9ad3ad051b3cb
Ingvix <arvi.sulin@hotmail.com>
2017-10-29 22:22:00 +0200

    English tranlation file for preference

---
+++ sailfishos-patch-improved-notification-preview_eng_en.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name/>
+	<message>
+        <source>sailfishos-patch-improved-notification-preview-he-notification_preview</source>
+        <translation>Notification preview</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-he-look</source>
+        <translation>Look</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-notification_style</source>
+        <translation>Style:</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-all_round</source>
+        <translation>All-round</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-all_sharp</source>
+        <translation>All-sharp</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-speech_bubble_left</source>
+        <translation>Speech bubble (left)</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-speech_bubble_right</source>
+        <translation>Speech bubble (right)</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-sticky_corners</source>
+        <translation>Sticky corners</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-corner_radius</source>
+        <translation>Corner radius</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-margins</source>
+        <translation>Margins</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-position</source>
+        <translation>Position:</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-position_center</source>
+        <translation>Center</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-position_left</source>
+        <translation>Left</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-position_right</source>
+        <translation>Right</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-min_width</source>
+        <translation>Minimum width</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-max_width</source>
+        <translation>Maximum width</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-of_landscape_width</source>
+        <translation>* Of landscape width</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-he-behavior</source>
+        <translation>Behavior</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-duration</source>
+        <translation>Duration</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-tap_action</source>
+        <translation>Tap:</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-none_action</source>
+        <translation>None</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-notification_action</source>
+        <translation>Notification action</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-dismiss_preview</source>
+        <translation>Dismiss preview</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-remove_notification</source>
+        <translation>Remove notification</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-left_swipe_action</source>
+        <translation>Left swipe:</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-right_swipe_action</source>
+        <translation>Right swipe:</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-down_swipe_action</source>
+        <translation>Down swipe:</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-me-preview</source>
+        <translation>Preview</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-preview_body</source>
+        <translation>Preview body</translation>
+    </message>
+    <message>
+        <source>sailfishos-patch-improved-notification-preview-la-preview_summary</source>
+        <translation>Preview summary</translation>
+    </message>
+</context>
+</TS>
